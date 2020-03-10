3380726e2bc89e95d6f24fe4129060a45198f048
Peter Tribe <ptribe@pnimedia.com>
2020-03-09 18:24:38 -0700

    Added: ulong support

---
+++ src/ImageSharp.Textures/Templates/R32G32B32A32_FLOAT.cs
@@ -0,0 +1,188 @@
+// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace SixLabors.ImageSharp.Textures.PixelFormats
+{
+    /// <summary>
+    /// Pixel type containing four 32-bit unsigned normalized values ranging from 0 to 0.
+    /// The color components are stored in red, green, blue, alpha order (least significant to most significant float).
+    /// <para>
+    /// Ranges from [0, 0, 0, 0] to [1, 1, 1, 1] in vector form.
+    /// </para>
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public partial struct R32G32B32A32_FLOAT : IPixel<R32G32B32A32_FLOAT>
+    {
+        /// <summary>
+        /// Gets or sets the red component.
+        /// </summary>
+        [FieldOffset(0)]
+        public float R;
+
+        /// <summary>
+        /// Gets or sets the green component.
+        /// </summary>
+        [FieldOffset(4)]
+        public float G;
+
+        /// <summary>
+        /// Gets or sets the blue component.
+        /// </summary>
+        [FieldOffset(8)]
+        public float B;
+
+        /// <summary>
+        /// Gets or sets the alpha component.
+        /// </summary>
+        [FieldOffset(12)]
+        public float A;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="R32G32B32A32_FLOAT"/> struct.
+        /// </summary>
+        /// <param name="r">The red component.</param>
+        /// <param name="g">The green component.</param>
+        /// <param name="b">The blue component.</param>
+        /// <param name="a">The alpha component.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public R32G32B32A32_FLOAT(float r, float g, float b, float a)
+        {
+            this.R = r;
+            this.G = g;
+            this.B = b;
+            this.A = a;
+        }
+
+        /// <summary>
+        /// Compares two <see cref="R32G32B32A32_FLOAT"/> objects for equality.
+        /// </summary>
+        /// <param name="left">The <see cref="R32G32B32A32_FLOAT"/> on the left side of the operand.</param>
+        /// <returns>
+        /// True if the <paramref name="left"/> parameter is equal to the <paramref name="right"/> parameter; otherwise, false.
+        /// </returns>
+        /// <param name="right">The <see cref="R32G32B32A32_FLOAT"/> on the right side of the operand.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(R32G32B32A32_FLOAT left, R32G32B32A32_FLOAT right) => left.Equals(right);
+
+        /// <summary>
+        /// Compares two <see cref="R32G32B32A32_FLOAT"/> objects for equality.
+        /// </summary>
+        /// <param name="left">The <see cref="R32G32B32A32_FLOAT"/> on the left side of the operand.</param>
+        /// <param name="right">The <see cref="R32G32B32A32_FLOAT"/> on the right side of the operand.</param>
+        /// <returns>
+        /// True if the <paramref name="left"/> parameter is not equal to the <paramref name="right"/> parameter; otherwise, false.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(R32G32B32A32_FLOAT left, R32G32B32A32_FLOAT right) => !left.Equals(right);
+
+        /// <inheritdoc />
+        public PixelOperations<R32G32B32A32_FLOAT> CreatePixelOperations() => new PixelOperations<R32G32B32A32_FLOAT>();
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromScaledVector4(Vector4 vector) => this.FromVector4(vector);
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Vector4 ToScaledVector4() => this.ToVector4();
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromVector4(Vector4 vector)
+        {
+            this.R = vector.X;
+            this.G = vector.Y;
+            this.B = vector.Z;
+            this.A = vector.W;
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Vector4 ToVector4()
+        {
+            return new Vector4(
+                this.R,
+                this.G,
+                this.B,
+                this.A);
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromBgra5551(Bgra5551 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromArgb32(Argb32 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromBgr24(Bgr24 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromBgra32(Bgra32 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromL8(L8 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromL16(L16 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromLa16(La16 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromLa32(La32 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromRgb24(Rgb24 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromRgba32(Rgba32 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void ToRgba32(ref Rgba32 dest)
+        {
+            dest.FromScaledVector4(this.ToScaledVector4());
+        }
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromRgb48(Rgb48 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromRgba64(Rgba64 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) => obj is R32G32B32A32_FLOAT other && this.Equals(other);
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(R32G32B32A32_FLOAT other) => this.R.Equals(other.R) && this.G.Equals(other.G) && this.B.Equals(other.B) && this.A.Equals(other.A);
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return FormattableString.Invariant($"R32G32B32A32_FLOAT({this.R}, {this.G}, {this.B}, {this.A})");
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override int GetHashCode() => HashCode.Combine(this.R, this.G, this.B, this.A);
+    }
+}
---
+++ src/ImageSharp.Textures/Templates/R32G32B32A32_UINT.cs
@@ -0,0 +1,188 @@
+// Copyright (c) Six Labors and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace SixLabors.ImageSharp.Textures.PixelFormats
+{
+    /// <summary>
+    /// Pixel type containing four 32-bit unsigned normalized values ranging from 0 to 0.
+    /// The color components are stored in red, green, blue, alpha order (least significant to most significant uint).
+    /// <para>
+    /// Ranges from [0, 0, 0, 0] to [1, 1, 1, 1] in vector form.
+    /// </para>
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public partial struct R32G32B32A32_UINT : IPixel<R32G32B32A32_UINT>
+    {
+        /// <summary>
+        /// Gets or sets the red component.
+        /// </summary>
+        [FieldOffset(0)]
+        public uint R;
+
+        /// <summary>
+        /// Gets or sets the green component.
+        /// </summary>
+        [FieldOffset(4)]
+        public uint G;
+
+        /// <summary>
+        /// Gets or sets the blue component.
+        /// </summary>
+        [FieldOffset(8)]
+        public uint B;
+
+        /// <summary>
+        /// Gets or sets the alpha component.
+        /// </summary>
+        [FieldOffset(12)]
+        public uint A;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="R32G32B32A32_UINT"/> struct.
+        /// </summary>
+        /// <param name="r">The red component.</param>
+        /// <param name="g">The green component.</param>
+        /// <param name="b">The blue component.</param>
+        /// <param name="a">The alpha component.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public R32G32B32A32_UINT(uint r, uint g, uint b, uint a)
+        {
+            this.R = r;
+            this.G = g;
+            this.B = b;
+            this.A = a;
+        }
+
+        /// <summary>
+        /// Compares two <see cref="R32G32B32A32_UINT"/> objects for equality.
+        /// </summary>
+        /// <param name="left">The <see cref="R32G32B32A32_UINT"/> on the left side of the operand.</param>
+        /// <returns>
+        /// True if the <paramref name="left"/> parameter is equal to the <paramref name="right"/> parameter; otherwise, false.
+        /// </returns>
+        /// <param name="right">The <see cref="R32G32B32A32_UINT"/> on the right side of the operand.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(R32G32B32A32_UINT left, R32G32B32A32_UINT right) => left.Equals(right);
+
+        /// <summary>
+        /// Compares two <see cref="R32G32B32A32_UINT"/> objects for equality.
+        /// </summary>
+        /// <param name="left">The <see cref="R32G32B32A32_UINT"/> on the left side of the operand.</param>
+        /// <param name="right">The <see cref="R32G32B32A32_UINT"/> on the right side of the operand.</param>
+        /// <returns>
+        /// True if the <paramref name="left"/> parameter is not equal to the <paramref name="right"/> parameter; otherwise, false.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(R32G32B32A32_UINT left, R32G32B32A32_UINT right) => !left.Equals(right);
+
+        /// <inheritdoc />
+        public PixelOperations<R32G32B32A32_UINT> CreatePixelOperations() => new PixelOperations<R32G32B32A32_UINT>();
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromScaledVector4(Vector4 vector) => this.FromVector4(vector);
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Vector4 ToScaledVector4() => this.ToVector4();
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromVector4(Vector4 vector)
+        {
+            this.R = (uint)(vector.X * 4294967295);
+            this.G = (uint)(vector.Y * 4294967295);
+            this.B = (uint)(vector.Z * 4294967295);
+            this.A = (uint)(vector.W * 4294967295);
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Vector4 ToVector4()
+        {
+            return new Vector4(
+                this.R / 4294967295F,
+                this.G / 4294967295F,
+                this.B / 4294967295F,
+                this.A / 4294967295F);
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromBgra5551(Bgra5551 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromArgb32(Argb32 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromBgr24(Bgr24 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromBgra32(Bgra32 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromL8(L8 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromL16(L16 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromLa16(La16 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromLa32(La32 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromRgb24(Rgb24 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromRgba32(Rgba32 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void ToRgba32(ref Rgba32 dest)
+        {
+            dest.FromScaledVector4(this.ToScaledVector4());
+        }
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromRgb48(Rgb48 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromRgba64(Rgba64 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) => obj is R32G32B32A32_UINT other && this.Equals(other);
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(R32G32B32A32_UINT other) => this.R.Equals(other.R) && this.G.Equals(other.G) && this.B.Equals(other.B) && this.A.Equals(other.A);
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return FormattableString.Invariant($"R32G32B32A32_UINT({this.R}, {this.G}, {this.B}, {this.A})");
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override int GetHashCode() => HashCode.Combine(this.R, this.G, this.B, this.A);
+    }
+}
