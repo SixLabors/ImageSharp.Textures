b0b59bbd3a3559f47236b2c451b292b6f21ff563
Brian Popow <popow@gmx.de>
2021-03-09 18:28:19 +0100

    Add Rgba5551 pixel format

---
+++ src/ImageSharp.Textures/PixelFormats/Generated/Rgba5551.cs
@@ -0,0 +1,179 @@
+// Copyright (c) Six Labors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace SixLabors.ImageSharp.Textures.PixelFormats
+{
+    /// <summary>
+    /// Packed pixel type containing unsigned normalized values ranging from 0 to 1.
+    /// The x, y, z and w components uses 5, 5, 5 and 1 bits respectively.
+    /// <para>
+    /// Ranges from [0, 0, 0, 0] to [1, 1, 1, 1] in vector form.
+    /// </para>
+    /// </summary>
+    public partial struct Rgba5551 : IPixel<Rgba5551>, IPackedVector<ushort>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Rgba5551"/> struct.
+        /// </summary>
+        /// <param name="x">The x-component</param>
+        /// <param name="y">The y-component</param>
+        /// <param name="z">The z-component</param>
+        /// <param name="w">The w-component</param>
+        public Rgba5551(float x, float y, float z, float w)
+            : this(new Vector4(x, y, z, w))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Rgba5551"/> struct.
+        /// </summary>
+        /// <param name="vector">
+        /// The vector containing the components for the packed vector.
+        /// </param>
+        public Rgba5551(Vector4 vector) => this.PackedValue = Pack(ref vector);
+
+        /// <inheritdoc/>
+        public ushort PackedValue { get; set; }
+
+        /// <summary>
+        /// Compares two <see cref="Rgba5551"/> objects for equality.
+        /// </summary>
+        /// <param name="left">The <see cref="Rgba5551"/> on the left side of the operand.</param>
+        /// <returns>
+        /// True if the <paramref name="left"/> parameter is equal to the <paramref name="right"/> parameter; otherwise, false.
+        /// </returns>
+        /// <param name="right">The <see cref="Rgba5551"/> on the right side of the operand.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(Rgba5551 left, Rgba5551 right) => left.Equals(right);
+
+        /// <summary>
+        /// Compares two <see cref="Rgba5551"/> objects for equality.
+        /// </summary>
+        /// <param name="left">The <see cref="Rgba5551"/> on the left side of the operand.</param>
+        /// <param name="right">The <see cref="Rgba5551"/> on the right side of the operand.</param>
+        /// <returns>
+        /// True if the <paramref name="left"/> parameter is not equal to the <paramref name="right"/> parameter; otherwise, false.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(Rgba5551 left, Rgba5551 right) => !left.Equals(right);
+
+        /// <inheritdoc />
+        public PixelOperations<Rgba5551> CreatePixelOperations() => new PixelOperations<Rgba5551>();
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromScaledVector4(Vector4 vector) => this.FromVector4(vector);
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Vector4 ToScaledVector4() => this.ToVector4();
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromVector4(Vector4 vector)
+        {
+            var vector4 = new Vector4(vector.X, vector.Y, vector.Z, vector.W);
+            this.PackedValue = Pack(ref vector4);
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Vector4 ToVector4()
+        {
+            return new Vector4(
+                (this.PackedValue & 31) / 31F,
+                ((this.PackedValue >> 5) & 31) / 31F,
+                ((this.PackedValue >> 10) & 31) / 31F,
+                ((this.PackedValue >> 15) & 1) / 1F);
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromBgra5551(Bgra5551 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromArgb32(Argb32 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromBgr24(Bgr24 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromBgra32(Bgra32 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromL8(L8 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromL16(L16 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromLa16(La16 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromLa32(La32 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromRgb24(Rgb24 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromRgba32(Rgba32 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void ToRgba32(ref Rgba32 dest)
+        {
+            dest.FromScaledVector4(this.ToScaledVector4());
+        }
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromRgb48(Rgb48 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromRgba64(Rgba64 source) => this.FromScaledVector4(source.ToScaledVector4());
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) => obj is Rgba5551 other && this.Equals(other);
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(Rgba5551 other) => this.PackedValue.Equals(other.PackedValue);
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            var vector = this.ToVector4();
+            return FormattableString.Invariant($"Rgba5551({vector.X:#0.##}, {vector.Y:#0.##}, {vector.Z:#0.##}, {vector.W:#0.##})");
+        }
+
+        /// <inheritdoc />
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override int GetHashCode() => this.PackedValue.GetHashCode();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static ushort Pack(ref Vector4 vector)
+        {
+            vector = Vector4.Clamp(vector, Vector4.Zero, Vector4.One);
+            return (ushort)(
+                ((uint)Math.Round(vector.X * 31F) & 31)
+                | (((uint)Math.Round(vector.Y * 31F) & 31) << 5)
+                | (((uint)Math.Round(vector.Z * 31F) & 31) << 10)
+                | (((uint)Math.Round(vector.W * 1F) & 1) << 15));
+        }
+    }
+}
---
+++ src/ImageSharp.Textures/TextureFormats/Decoding/Rgba5551.cs
@@ -0,0 +1,36 @@
+// Copyright (c) Six Labors.
+// Licensed under the Apache License, Version 2.0.
+
+namespace SixLabors.ImageSharp.Textures.TextureFormats.Decoding
+{
+    /// <summary>
+    /// Texture format for pixel data with 5 bit for each color channel and 1 bit for the alpha channel.
+    /// </summary>
+    internal struct Rgba5551 : IBlock<Rgba5551>
+    {
+        /// <inheritdoc/>
+        public int BitsPerPixel => 16;
+
+        /// <inheritdoc/>
+        public byte PixelDepthBytes => 2;
+
+        /// <inheritdoc/>
+        public byte DivSize => 1;
+
+        /// <inheritdoc/>
+        public byte CompressedBytesPerBlock => 2;
+
+        /// <inheritdoc/>
+        public bool Compressed => false;
+
+        /// <inheritdoc/>
+        public Image GetImage(byte[] blockData, int width, int height)
+        {
+            byte[] decompressedData = this.Decompress(blockData, width, height);
+            return Image.LoadPixelData<Textures.PixelFormats.Rgba5551>(decompressedData, width, height);
+        }
+
+        /// <inheritdoc/>
+        public byte[] Decompress(byte[] blockData, int width, int height) => blockData;
+    }
+}
